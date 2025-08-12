8b299c2df33480b8a191891d26d3a83f567a13f4
Rulli Wibowo <rulli.wibowo@gmail.com>
2025-08-12 13:48:37 +0700

    push 12.08.2025 13.48

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Login from "../pages/Login.vue";
-import EventList from "../pages/EventList.vue";
-import EventbyID from "../pages/EventbyID.vue";
-import Scan from "../pages/Scan.vue";
-import Report from "../pages/Report.vue";
-
 const routes = [
   { path: "/", redirect: "/events" },
-  { path: "/login", component: Login },
-  { path: "/events", component: EventList },
-  { path: "/events/:id", component: EventbyID },
-  { path: "/scan", component: Scan },
-  { path: "/report/:id", component: Report },
+  { path: "/login", component: () => import("../pages/Login.vue") },
+  { path: "/events", component: () => import("../pages/EventList.vue") },
+  { path: "/events/:id", component: () => import("../pages/EventbyID.vue") },
+  { path: "/scan", component: () => import("../pages/Scan.vue") },
+  { path: "/report/:id", component: () => import("../pages/Report.vue") },
   { path: "/:pathMatch(.*)*", redirect: "/" },
 ];
 
